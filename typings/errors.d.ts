fad55bf53fc4d00417ff07d74da2a4356f42395e
Gordon Sim <gsim@redhat.com>
2018-07-18 13:25:43 +0100

    add toJSON() to type definition

--- typings/errors.d.ts
+++ typings/errors.d.ts
@@ -9,10 +9,18 @@ export declare interface TypeError extends ProtocolError {
     message: string;
     name: string;
 }
+
+export interface SimpleError {
+  type: string;
+  code: string;
+  message: string
+}
+
 export declare interface ConnectionError extends Error {
     message: string;
     name: string;
     description: string;
     condition: any;
     connection: any;
+    toJSON(): SimpleError;//avoids circular reference by omitting connection
 }
