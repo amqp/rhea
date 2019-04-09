6e5b95e199b72e8158f38ef50fa9057cbbbb636a
Shivangi Reja <shreja@microsoft.com>
2019-04-09 10:42:55 -0700

    Add argument types that are passed to ws.connect.

--- typings/ws.d.ts
+++ typings/ws.d.ts
@@ -1,7 +1,28 @@
 /// <reference types="node" />
 
+/**
+ * Describes the required shape of WebSocket instances.
+ * @interface WebSocketInstance
+ */
+export interface WebSocketInstance {
+  send: Function;
+  onmessage: Function | null;
+  onopen: Function | null;
+  close: Function | null;
+  onclose: Function | null;
+  onerror: Function | null;
+}
+
+/**
+ * Describes the required shape of WebSocket constructors.
+ * @interface WebSocketImpl
+ */
+export interface WebSocketImpl {
+  new(url: string, protocols?: string | string[], options?: any): WebSocketInstance;
+}
+
 export namespace ws {
-  export type connect = (Impl: any) => (url: any, protocols: any, options: any) => () => {
+  export type connect = (Impl: WebSocketImpl) => (url: string, protocols: string | string[], options: any) => () => {
     [x: string]: any;
     connect: (port_ignore: any, host_ignore: any, options_ignore: any, callback: Function) => {
       [x: string]: any;
